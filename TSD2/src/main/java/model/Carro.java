5f049d51ff144b0992830747e82ae893a6805522
mailonB <mailon.barbetta@gmail.com>
2024-10-10 21:44:51 -0300

    Tela e umas ideias gerais

---
+++ TSD2/src/main/java/model/Carro.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Mailon
+ */
+public class Carro extends Thread{ // primeira estrutura do carro
+    
+    public int id;
+    public String cor; // pensei em fazer um um array de corres pra escolher as corres aleatoriamente 
+    public int posx, posy; // coordenadas da matriz
+    public int tempo; // tempo de espera 
+    
+     public void run() {
+           try {
+              while (true) {
+              
+              
+              
+              
+              }
+             
+         } catch (Exception e) {
+             
+         }
+           
+        }
+     
+     public void escolheSaida(){
+         // to so pensando na estrutura kkkk
+     }
+     
+     public void escolheEntrada(){
+         // to so pensando na estrutura kkkk
+     }
+     
+     }
+
